2be62bcbf0eb2b2ad707b672dd41da9fe528cd79
brandon23567 <brandonmahuvava0@gmail.com>
2025-07-05 12:10:11 +0530

    adding the new files for testing

--- website/settings.py
+++ website/settings.py
@@ -62,10 +62,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'website.wsgi.application'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST_NAME"),
+        'PORT': os.getenv("DB_PORT_NUMBER"),
     }
 }
 
